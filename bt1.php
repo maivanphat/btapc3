f2486b323ed9e0c49466cb693a4a2f95bb245f21
maivanphat <maiphatns98@gmail.com>
2018-03-27 15:14:50 +0700

    Revert "btc3"
    This reverts commit 4e5da6d50480b743575eea4afa13ee7886cc344c.

--- bt1.php
+++
@@ -1,4 +0,0 @@
-<form action="hello.php" method="GET">
-    HoTen <input name="hoten" type="text" </input> <br><br> 
-    <button name="button" type="submit">SUBMIT</button>
-</form>
--- bt2.php
+++
@@ -1,9 +0,0 @@
-<form method="GET">
-    <input name="search" placeholder="Tu Khoa..." <input> <br><br>
-    <button name="tim" type="submit"> TIM</button>
-    
-</form>
-<?php
-       echo $_GET["search"];
- 
-   ?>
